92471161c2d5128999d1826baf57e3dd3247a2dc
Nigoulash <g.n.oneill@outlook.com>
2024-11-27 15:35:37 +0100

    newbigmerge och några scripts

---
+++ Assets/AltLevelStarter.cs
@@ -0,0 +1,17 @@
+using TMPro;
+using UnityEngine;
+
+public class AltLevelStarter : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        GlobalMovement.canMove = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
