4ebff01570db3c07a076be9263c99a293133cc31
thejakethesnake123 <jacobnilssoon@gmail.com>
2024-11-27 15:00:19 +0100

    IDK

--- Assets/Scripts/Variables/ObstacleCollision.cs
+++ Assets/Scripts/Variables/ObstacleCollision.cs
@@ -1,14 +1,38 @@
-using TMPro.Examples;
 using UnityEngine;
 
 public class ObstacleCollision : MonoBehaviour
 {
     public GameObject thePlayer;
+    //public bool isDead
+    //{ get
+    //    {
+    //        return Health == 0;
+    //    }
+    //}
+
+
+    //public GameObject charModel;
+    GameObject rigidBody;
 
     void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Enter");
-        this.gameObject.GetComponent<BoxCollider>().enabled = false;
-        GlobalMovement.canMove = false;
+        if(other.tag == "Player")
+        {
+            GlobalMovement.canMove = false;
+            //charModel.GetComponent<Animator>().Play("Death");
+
+        }
+
+
+        //if(isDead)
+        //{
+        //    _animator.SetTrigger("death");
+        //}
+        //Debug.Log("Enter");
+        //this.gameObject.GetComponent<BoxCollider>().enabled = false;
+        //thePlayer.GetComponent<PlayerMovement>().enabled = false;
+
+        //GlobalMovement.canMove = false;
     }
 }
+
