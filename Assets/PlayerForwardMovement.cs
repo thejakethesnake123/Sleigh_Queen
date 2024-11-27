b86e452958299bc3d966b5c910d72fb798b2a429
Nigoulash <g.n.oneill@outlook.com>
2024-11-27 11:42:34 +0100

    public bool playerMove

--- Assets/PlayerForwardMovement.cs
+++ Assets/PlayerForwardMovement.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PlayerForwardMovement : MonoBehaviour
 {
     public float moveSpeed = 10f;
+    public bool playerMove = true;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -10,9 +11,34 @@ public class PlayerForwardMovement : MonoBehaviour
         
     }
 
+    void MoveSpeed()
+    {
+        if (playerMove == true)
+        {
+            moveSpeed = 10f;
+        }
+
+        if (playerMove == false)
+        {
+            moveSpeed = 0f;
+        }
+    }
+
+    void PlayerPosition()
+    {
+        if (transform.position == new Vector3(0f, 20.4f, 100f))
+        {
+            playerMove = false;
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
+        MoveSpeed();
+
+        PlayerPosition();
+
         transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed, Space.World);
     }
 }
